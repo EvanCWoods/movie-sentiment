d9bd5ffdbd19236ba8712378a50df48fd4c97749
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2021-05-27 12:43:26 +1000

    Update model_update_1.py

--- model_update_1.py
+++ model_update_1.py
@@ -1,1 +1,72 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
+imdb = tf.keras.datasets.imdb
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data()
+
+len(train_data[0]), len(train_data[1])
+
+train_data.shape
+
+
+
+word_index = imdb.get_word_index()
+
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index['<PAD>'] = 0
+word_index['<START>'] = 1
+word_index['<UNK>'] = 2
+word_index['<UNUSED>'] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+def decode_review(text):
+  return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+
+
+decode_review(train_data[0])
+
+
+train_data = tf.keras.preprocessing.sequence.pad_sequences(train_data,
+                                                         value=word_index['<PAD>'],
+                                                         padding='post',
+                                                         maxlen=256)
+test_data = tf.keras.preprocessing.sequence.pad_sequences(train_data,
+                                                        value=word_index['<PAD>'],
+                                                        padding='post',
+                                                        maxlen=256)
+len(train_data[0]), len(train_data[1])
+
+
+tf.random.set_seed(42)
+
+vocab_size = 10000
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Embedding(vocab_size, 16),
+  tf.keras.layers.GlobalAveragePooling1D(),
+  tf.keras.layers.Dense(16, activation='relu'),
+  tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+partial_y_train = train_labels[10000:]
